bce93b9df4178e7bd3882b0660c65bdb2f743673
Alyona Markina <alyona.markina.fipi@gmail.com>
2017-06-26 01:22:00 +0300

    lab 5-6

---
+++ Assets/Scripts/SettingsPopUP.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+
+public class SettingsPopUP : MonoBehaviour {
+	public  SettingsPopUP playButton;
+	public UnityEvent signalOnClick = new UnityEvent();
+	public GameObject settingPrefab;
+	public void _onClick()
+	{
+		this.signalOnClick.Invoke();
+	}
+
+	void Start()
+	{
+		playButton.signalOnClick.AddListener(this.showSetting);
+	}
+	void showSetting()
+	{
+		GameObject parent = UICamera.first.transform.parent.gameObject;
+		GameObject obj = NGUITools.AddChild (parent, settingPrefab);
+		SettingsPopUP popup = obj.GetComponent<SettingsPopUP> ();
+		obj.transform.localScale = new Vector3 (0.52f, 0.54f, 1f);
+	}
+		}
